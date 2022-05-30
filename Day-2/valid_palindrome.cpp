3bab3c7b25cb06b966c0e68ad10fbe48d8f42161
Tarun Sharma <36966951+TarunnSharmaa@users.noreply.github.com>
2022-05-30 14:34:09 +0530

    Create valid_palindrome.cpp

---
+++ Day-2/valid_palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string A) {
+        int s=0;
+        int n = A.size();
+        int e = n-1;
+        
+        while(s<=e){
+            
+            while(s<e && !isalnum(A[s])) s++;
+            while(s<e && !isalnum(A[e])) e--;
+            
+            if(toupper(A[s]) != toupper(A[e])) return false;
+            
+            s++;
+            e--;
+        }
+        return true;
+    }
+};
